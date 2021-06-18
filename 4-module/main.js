6989372d1850b819dcfc1039f2d7ac50089d13c2
happysubin <76802855+happysubin@users.noreply.github.com>
2021-06-18 16:17:03 +0900

    node module

---
+++ 4-module/main.js
@@ -0,0 +1,12 @@
+//import { increase, getCount } from "./counter";
+const counter = require("./counter");
+/*counter.increase();
+counter.increase();
+counter.increase();*/
+console.log(counter.getCount());
+
+/*console.clear();
+increase();
+increase();
+getCount();
+*/
