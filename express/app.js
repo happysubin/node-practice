a35b085eb6222926b993244be29a963ce0e17f59
happysubin <76802855+happysubin@users.noreply.github.com>
2021-06-26 14:33:06 +0900

    express basic 2 (handle error)

--- express/app.js
+++ express/app.js
@@ -1,32 +1,43 @@
 import express from "express";
+import fs, { fstatSync } from "fs";
 
 const app = express();
 
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("first");
-    next();
-  },
-  (req, res, next) => {
-    console.log("first2");
-    next();
-  }
-);
+app.use(express.json()); //이 미들웨어를 통해 req.body 사용가능!!!
 
-app.use("/user/:id", (req, res) => {
-  console.log(req.query);
-  console.log(req.params.id);
-  res.setHeader("key", "value");
-  //res.sendStatus(400);
-  res.json({ name: "hello" });
-  //res.status(200).send("hi");
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
+
+app.get("/file1", (req, res) => {
+  try {
+    const data = fs.readFileSync("text1.txt");
+    res.send(data);
+  } catch {
+    res.sendStatus(404);
+  }
+});
+
+app.get("/file2", (req, res) => {
+  fs.promises
+    .readFile("text2.txt")
+    .then((data) => res.send(data))
+    .catch((error) => res.sendStatus(404));
+});
+
+app.get("/file3", async (req, res) => {
+  try {
+    const data = await fs.promises.readFile("file3.txt");
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
 });
 
 app.use((err, req, res, next) => {
   console.log(error);
   res.status(500).send("sorry,try later");
 });
+
 app.listen("3000");
-//ip
-//port
